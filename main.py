890bad385b75319a80612ac3a5cbe0976d87cd57
Dmytro Kuzmenko <marder1234567@gmail.com>
2024-10-11 08:46:33 +0300

    add gitignore

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+import os
 
 from app.handlers import router
 
+# Загружаем переменные окружения из .env файла
+load_dotenv()
+
 async def main():
-	bot = Bot(token='7155726021:AAEqHWrfhK5ufGl8j9nUj2tENGKA8B3tGDk')
-	dp = Dispatcher()
-	dp.include_router(router)
-	await dp.start_polling(bot)
-		
+    bot = Bot(token=os.getenv('TELEGRAM_BOT_TOKEN'))  # Получаем токен из переменных окружения
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
-	try:
-		asyncio.run(main())
-	except KeyboardInterrupt:
-		print('Бот выключен')
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен')
